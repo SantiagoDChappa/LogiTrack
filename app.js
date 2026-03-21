8157c83d9087feaa454f97896fb72d041a0f0c16
SantiagoDChappa <santiagodchappa@gmail.com>
2026-03-21 10:09:50 -0300

    [SCRUM-80]: Se corrigieron archivos que provenian de un switch entre ramas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path    = require('path');
 const app     = express();
 const port    = 3000;
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
@@ -10,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/',                 (req, res) => res.render('index'));
+app.get('/',                 (req, res) => res.render('index.ejs'));
 app.get('/dashboard',        (req, res) => res.render('dashboard'));
 app.get('/shipments',        (req, res) => res.render('shipment/index'));
 app.get('/shipments/new',    (req, res) => res.render('shipment/new'));
