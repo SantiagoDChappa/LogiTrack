40d0c16722dc8d15b2aaf38ab178b9d4c914913f
SantiagoDChappa <santiagodchappa@gmail.com>
2026-03-22 19:05:08 -0300

    [SCRUM-80]: Se arreglo erro de tabla de envios que aparecia el telefono en vez del documento

--- src/middlewares/shipment.js
+++ src/middlewares/shipment.js
@@ -1,13 +1,17 @@
 const {body, validationResult} = require("express-validator");
+const path = require('path');
+const fs = require('fs');
 
 const validateShipment = [
     body('remitente_nombre').notEmpty().trim().withMessage('El nombre del remitente es obligatorio'),
     body('remitente_email').isEmail().normalizeEmail().withMessage('Email del remitente inválido'),
     body('remitente_telefono').isLength(10).withMessage('Teléfono inválido'),
+    body('remitente_documento').isLength(10).withMessage('Teléfono inválido'),
 
     body('destinatario_nombre').notEmpty().trim().withMessage('El nombre del remitente es obligatorio'),
     body('destinatario_email').isEmail().normalizeEmail().withMessage('Email del remitente inválido'),
     body('destinatario_telefono').isLength(10).withMessage('Teléfono inválido'),
+    body('destinatario_documento').isLength(10).withMessage('Teléfono inválido'),
 
     body('calle').notEmpty().withMessage('La calle es obligatorio'),
     body('numero').notEmpty().withMessage('La numeracion es obligatorio'),
@@ -17,9 +21,23 @@ const validateShipment = [
 
 const handleValidationErrors = (req, res, next) => {
     const errors = validationResult(req);
+    const dataPath = path.join(__dirname, '../data/shipments.json')
+    const data =  JSON.parse(fs.readFileSync(dataPath, 'utf8'));    
+    
+    // Me obtengo el documento del formulario y verifico que no exista
+    const documentoDestinatario = req.body.destinatario_documento;
+    const documentoRemitente = req.body.remitente_documento;
+    if(data.find(shipment => shipment.destinatario.documento === documentoDestinatario)){
+        return res.render('shipment/new', { errors: errors.array(), body: req.body });
+    }else if(data.find(shipment => shipment.remitente.documento === documentoRemitente)){
+        return res.render('shipment/new', { errors: errors.array(), body: req.body });
+    }
+
     if (!errors.isEmpty()) {
         return res.render('shipment/new', { errors: errors.array(), body: req.body });
     }
+    
+    
     next();
 };
 
