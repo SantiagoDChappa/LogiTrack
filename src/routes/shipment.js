1e42638e22a0e1068fd7607314922fc8f8c77316
SantiagoDChappa <santiagodchappa@gmail.com>
2026-03-22 22:14:21 -0300

    [SCRUM-92]: Se acomodo listado con filtros. Ademas se agrego opcion de detalle de envio

--- src/routes/shipment.js
+++ src/routes/shipment.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getIndex, getDetail, getNewShipmentForm, createShipment } = require('../controllers/shipment.js');
+const { getIndex, getDetail, getNewShipmentForm, createShipment, searchShipments } = require('../controllers/shipment.js');
 const { validateShipment, handleValidationErrors } = require('../middlewares/shipment.js');
 
 router.get('/', getIndex)
+router.get('/search', searchShipments)
 router.get('/detail', getDetail)
 router.get('/new', getNewShipmentForm)
 router.post('/new', validateShipment, handleValidationErrors, createShipment)
