01d3ebd9333eb3943be5aa9a1f5d6cb40bcba2f5
SantiagoDChappa <santiagodchappa@gmail.com>
2026-03-26 16:59:01 -0300

    [Develop]: Se integro la base de datos para el manejo de informacion en el sistema. Ademas se termino de agregar el CRUD de Envio

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const path    = require('path');
 const app     = express();
 const port    = process.env.PORT || 3000;
+const sequelize = require('./src/database/connection');
+
 const homeRoutes = require('./src/routes/home')
 const shipmentRoutes = require('./src/routes/shipment')
+
+// Conecto la base de datos con el sistema
+sequelize.sync({ alter: true }) 
+    .then(() => console.log('Base de datos conectada y sincronizada'))
+    .catch(err => console.error('Error de DB:', err));
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
@@ -14,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', homeRoutes);
-app.use('/shipments', shipmentRoutes);
+app.use('/shipment', shipmentRoutes);
 
 app.listen(port, () => {
     console.log(`LogiTrack running at http://localhost:${port}`);
